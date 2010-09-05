4e832c3e4c188eac6a75b8165942a49ba43a01ce
catalin.strimbei <catalin.strimbei@f7f365bf-cd1a-e18e-2544-695ceb87d913>
2010-09-05 20:21:14 +0000

    mentenace

---
+++ EntitatiContabilitate/src/app/model/contabilitate/InregistrareDebit_.java
@@ -0,0 +1,11 @@
+package app.model.contabilitate;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2010-09-05T20:51:44.800+0300")
+@StaticMetamodel(InregistrareDebit.class)
+public class InregistrareDebit_ extends InregistrareContabila_ {
+	public static volatile SingularAttribute<InregistrareDebit, Integer> nrOrdineDebit;
+}
