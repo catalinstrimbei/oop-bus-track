7bcb4ca195ba1bdf3e00c9a60ca7b2eb08445e10
catalin.strimbei <catalin.strimbei@f7f365bf-cd1a-e18e-2544-695ceb87d913>
2010-09-08 09:07:52 +0000

    Rezolvat problema navigare

--- FacesContabilitate/src/app/contabilitate/ui/beans/FormConturi.java
+++ FacesContabilitate/src/app/contabilitate/ui/beans/FormConturi.java
@@ -7,6 +7,11 @@ import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.event.ActionEvent;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -16,44 +21,39 @@ import app.model.contabilitate.conturi.Cont;
 
 //@ManagedBean
 //@SessionScoped
-public class FormConturi {
+public class FormConturi implements Converter{
 	
 	private RegistruConturi registruConturi;
-	private List<Cont> conturi;
 	private Cont cont = null;
-	private Map<String, Object> conturiSE = null;
+	private Map<String, Object> conturi = null;
 	
 	public FormConturi() {
         // creare entity manager
         EntityManagerFactory emf = Persistence.
                 createEntityManagerFactory("EntitatiContabilitate");
         EntityManager em = emf.createEntityManager();
+        
         // initializare registru
         registruConturi = new RegistruConturi(em);
-        this.conturi = new ArrayList<Cont>(registruConturi.getConturi());
         
         // initializare conturi
-        if (this.conturi != null && !this.conturi.isEmpty()){
-        	this.cont = this.conturi.get(0);
-        	this.selectedCont = this.cont;
-        }
-        
-        // initializare conturi
-        conturiSE = new LinkedHashMap<String, Object>();
-        for (Cont c : this.conturi){
-        	conturiSE.put(c.getDenumire(), c);
+        conturi = new LinkedHashMap<String, Object>();
+        List<Cont> lstConturi = new ArrayList<Cont>(registruConturi.getConturi());
+        if (lstConturi != null && !lstConturi.isEmpty()){
+        	// initializare cont curent implicit
+        	this.cont = lstConturi.get(0);
+	        for (Cont c : lstConturi){
+	        	conturi.put(c.getDenumire(), c);
+	        	//System.out.println("Load Cont: " + c.getDenumire());
+	        }
         }
 	}
 	
-	public List<Cont> getConturi() {
-		return conturi;
-	}
 	
-	/*
-	public void setConturi(List<Cont> conturi) {
-		this.conturi = conturi;
+	public Map<String, Object> getConturi() {
+		//System.out.println("Get conturi - count " + this.conturi.size());
+		return this.conturi;
 	}
-	*/
 
 	public Integer getConturiCount(){
 		return conturi.size();
@@ -66,24 +66,49 @@ public class FormConturi {
 
 	public void setCont(Cont cont) {
 		this.cont = cont;
+		System.out.println("Selected: " + this.cont.getDenumire());
 	}	
 	
-	public Map<String, Object> getConturiSE(){
-		return this.conturiSE;
-	}
-	
-	//------------------------------------
-	private Cont selectedCont;
-
-	public Cont getSelectedCont() {
-		System.out.println("Get Selected: " + this.selectedCont.getDenumire());
-		return selectedCont;
+	//---------------------------------------
+	// Converter pt cboClienti - componenta de navigare
+	public Object getAsObject(FacesContext arg0, UIComponent arg1, String uiValue)
+			throws ConverterException {
+		return this.conturi.get(uiValue);
 	}
 
-	public void setSelectedCont(Cont selectedCont) {
-		this.selectedCont = selectedCont;
-		System.out.println("Selected: " + this.selectedCont.getDenumire());
+	public String getAsString(FacesContext arg0, UIComponent arg1, Object value)
+			throws ConverterException {
+		// TODO: tb tratat cazul cind value este null
+		return ((Cont)value).getDenumire();
+	}	
+	
+	//------------------------------------------
+	// Rol-Controller: actiuni formular
+	
+	// Actiuni de navigare
+	public void previousClient(ActionEvent evt){
+		System.out.println("Action previous");
+		
+		List<Cont> contList = new ArrayList(this.conturi.values());
+		int currentIdx = contList.indexOf(this.cont);
+		
+		if (currentIdx > 0)
+			this.cont = contList.get(currentIdx - 1);
 	}
 	
+	public void nextClient(ActionEvent evt){
+		System.out.println("Action next");
+		
+		List<Cont> contList = new ArrayList(this.conturi.values());
+		int currentIdx = contList.indexOf(this.cont);
+		
+		if ((currentIdx + 1) < contList.size())
+			this.cont = contList.get(currentIdx + 1);
+		
+	}
 	
+	// Actiuni tranzactionale
+	
+	//---------------------------------------------
+	// TODO: rubrica cboSubClasaConturi - legare la sursa de date (fieldul formConturi.cont.subClasaConturi) si sincronizare
 }
