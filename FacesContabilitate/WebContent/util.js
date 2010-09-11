5541732b58e598a1ba0f684e25895b3c07a3c72c
catalin.strimbei <catalin.strimbei@f7f365bf-cd1a-e18e-2544-695ceb87d913>
2010-09-11 09:54:15 +0000

    FormSimplu 0.7.1

--- FacesContabilitate/WebContent/util.js
+++ FacesContabilitate/WebContent/util.js
@@ -26,24 +26,30 @@ function getRow(element){
 	var currentRow = element.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
 	return currentRow;
 }
-function dataTableSelectUIComponent(component) {
-	alert("test");
-	
-	var id = component.name.substring(component.name.lastIndexOf(':'));
-    var el = component.form.elements;
 
+//dataTableSelectRow
+function dataTableSelectRow(radio) {
+	
+	var id = radio.name.substring(radio.name.lastIndexOf(':'));
+    var el = radio.form.elements;
+    var next_el_id = radio.name.substring(0, radio.name.lastIndexOf(':')) + ":txtNr";
+    var next_el = document.getElementById(next_el_id);
+    var btnActionSelect_id = radio.name.substring(0, radio.name.lastIndexOf(':')) + ":btnSelect";
+    var btnActionSelect = document.getElementById(btnActionSelect_id);
     
-    alert(component);
-    /*
-    alert(radio.name);    
-    alert(next_el_id);
-    */
+    //alert(btnActionSelect);
+    
     for (var i = 0; i < el.length; i++) {
         if (el[i].name.substring(el[i].name.lastIndexOf(':')) == id) {
-        	if (el[i].name != component.name){
+        	if (el[i].name != radio.name){
+            	el[i].checked = false;
             	getRow(el[i]).className = "";
         	}
         }
     }
-    getRow(component).className = "rowHighlighted";    
+    
+    getRow(radio).className = "rowHighlighted";
+    next_el.focus();
+    btnActionSelect.click();
+    
 }
