8dcaf55b36215bff65bc0c697875580a17b4ad44
catalin.strimbei <catalin.strimbei@f7f365bf-cd1a-e18e-2544-695ceb87d913>
2012-09-03 12:41:26 +0000

    Initial share

---
+++ trunk/TrackJavaScript/Start_OOJS/WebContent/Test.js
@@ -0,0 +1,30 @@
+includeJS("C1.js");
+includeJS("C2.js");
+
+function main(){
+	// test();
+	// testCreareObiecte();
+	// testCreareClase();
+	// testSiruri();
+	// testDate();
+	testExceptions();
+}
+
+function test(){
+	var test = "Test JavaScript!";
+	alert(test);
+	console.log(test)
+};
+
+function includeJS(jsPath){
+	  var script = document.createElement("script");
+	  script.setAttribute("type", "text/javascript");
+	  script.setAttribute("src", jsPath);
+	  document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+function IncludeJavaScript(jsFile)
+{
+  document.write('<script type="text/javascript" src="'
+    + jsFile + '"></scr' + 'ipt>'); 
+}
