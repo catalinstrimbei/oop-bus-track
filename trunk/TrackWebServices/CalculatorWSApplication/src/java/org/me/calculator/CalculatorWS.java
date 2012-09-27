906a74679ef3e4120c0e2ab7c7fbfc0bcb93ab47
catalin.strimbei@gmail.com <catalin.strimbei@gmail.com@f7f365bf-cd1a-e18e-2544-695ceb87d913>
2012-09-27 10:22:14 +0000

    Initial - JBoss prepared

---
+++ trunk/TrackWebServices/CalculatorWSApplication/src/java/org/me/calculator/CalculatorWS.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.me.calculator;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author catalin.strimbei
+ */
+@WebService()
+@Stateless()
+public class CalculatorWS {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "add")
+    public int add(@WebParam(name = "i") int i, @WebParam(name = "j") int j) {
+        int k = i + j;
+        return k;
+    }
+}
