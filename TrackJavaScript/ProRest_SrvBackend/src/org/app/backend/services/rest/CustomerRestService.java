35aacba2b268be166f94f4fc62bda1b729251894
catalin.strimbei <catalin.strimbei@gmail.com>
2013-02-20 20:53:22 +0000

    POST Request

--- TrackJavaScript/ProRest_SrvBackend/src/org/app/backend/services/rest/CustomerRestService.java
+++ TrackJavaScript/ProRest_SrvBackend/src/org/app/backend/services/rest/CustomerRestService.java
@@ -3,7 +3,10 @@ package org.app.backend.services.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -64,6 +67,17 @@ public class CustomerRestService {
 	  customers.add(new Customer(1003l, "Third Customer"));
 	  return customers;
   }  
+  
+  @POST
+  @Path("/json/savecustomer")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Customer saveJSONCustomer(Customer jsonCustomer){
+	  System.out.println("JSON ID: " + jsonCustomer.getId());
+	  System.out.println("JSON Name: " + jsonCustomer.getName());
+	  jsonCustomer.setName(jsonCustomer.getName() + "_");
+	  return jsonCustomer;
+  }    
 } 
 
 class Customer{
