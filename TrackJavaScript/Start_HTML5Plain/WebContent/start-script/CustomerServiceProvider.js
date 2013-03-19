c450e52b4d1b9b1a65bfb1ea52827e2eb2b75e59
catalin.strimbei <catalin.strimbei@gmail.com>
2013-03-19 20:36:46 +0000

    Update MVC Cotrollers

--- TrackJavaScript/Start_HTML5Plain/WebContent/start-script/CustomerServiceProvider.js
+++ TrackJavaScript/Start_HTML5Plain/WebContent/start-script/CustomerServiceProvider.js
@@ -4,23 +4,22 @@ function CustomerService(url){
 	this.urlRestBase = url;
 	
 	// service operations/functions - map to REST operations
-	this.getCustomers =  function(){
+	this.getCustomers =  function(dataHandler){
 		var urlRest = urlRestBase + "getcustomers";
-		var customers;
 		var reqRest = new XMLHttpRequest();
 		reqRest.onload = function(){
 			if (reqRest.status == 200){
 				console.log("REST Response: " + reqRest.responseText);
 				// populate selectCustomers navigator combo from REST response
 				customers = JSON.parse(reqRest.responseText);
+				// callback
+				dataHandler(customers);
 			}else{
 				console.log("REST Request problem or ERROR ");
 			}
 		};
 		reqRest.open("GET", urlRest);
-		reqRest.send(null);
-		console.log(" - getCustomers .... " + customers);
-		return customers;
+		reqRest.send(null);		
 	}
 	this.saveCustomers =  function(){
 		// TODO
