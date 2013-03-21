37a449a0e163bd11225830a380d06c65064ffd97
catalin.strimbei@gmail.com <catalin.strimbei@gmail.com@f7f365bf-cd1a-e18e-2544-695ceb87d913>
2013-03-21 08:38:43 +0000

    Re-import

---
+++ trunk/TrackJavaScript/Start_HTML5/WebContent/start-script/BindingController.js
@@ -0,0 +1,91 @@
+/* Resposibilities
+ * - binding [data model] - to - [form fields] [synchronization]
+ */
+function bindingForm(){
+		console.log(" - bindingForm ... " + Date());
+		//document.getElementById("FormCustomer").disabled=false;
+		// bind data navigation component
+		bindDataBrowseComponent();
+		// bind form fields
+		bindFormFields();
+		
+		// enable form
+		disableFormContent(false);			
+}
+
+function bindDataBrowseComponent(){
+	if (customers){
+		console.log(" - bindDataBrowseComponent .... " + Date());
+		var selectCustomers = document.getElementById("customers");
+		// reset
+		while(selectCustomers.options.length > 0){
+			selectCustomers.removeChild(selectCustomers.options[0]);
+		}
+		// repopulate
+		for(var i = 0; i < customers.length; i++) {
+		    var customerElement = document.createElement("option");
+		    customerElement.textContent = customers[i].name;
+		    customerElement.value = customers[i].id;
+		    selectCustomers.appendChild(customerElement);
+		}		
+		customer = customers[0];		
+	}	
+}
+
+function bindFormFields(){
+	if (customer){
+		console.log(" - bindFormFields .... " + Date());
+		document.getElementById("customerID").value = customer.id;
+		document.getElementById("customerName").value = customer.name;				
+		document.getElementById("registrationDate").value = customer.registrationDate;
+		// log
+		document.getElementById("response").innerHTML = customers[0].id + " - " + customers[0].name;		
+	}	
+}
+
+function selectCurrentCustomer(){
+	var selectCustomers = document.getElementById("customers");
+	var currentId = selectCustomers.value;
+	console.log("selectCurrentCustomer - currentId = " + currentId + " in " + customers);
+	if(currentId){
+		for(var i = 0; i < customers.length; i++) {
+			if (customers[i].id == currentId)
+				customer = customers[i];
+		}
+		// refresh form
+		console.log("customer.id = " + customer.id);
+		document.getElementById("customerID").value = customer.id;
+		document.getElementById("customerName").value = customer.name;	
+	}
+}
+
+function selectCurrentCustomerAsync(){
+	
+	var selectCustomers = document.getElementById("customers");
+	var currentId = selectCustomers.value;
+	console.log("selectCurrentCustomer - currentId = " + currentId + " in " + customers);
+	
+	if(currentId){		
+		/* using worker */
+		disableFormContent(true);
+		// disable form
+		var worker = new Worker("start-script/QueryDataCollectionWorker.js");
+		// prepare worker
+		worker.onmessage = function(event){
+			console.log("selectCurrentCustomer - worker done: " + event.data);
+			customer = event.data;
+			// refresh form
+			console.log("selectCurrentCustomer - customer.id = " + customer.id);
+			document.getElementById("customerID").value = customer.id;
+			document.getElementById("customerName").value = customer.name;
+			// enable form
+			disableFormContent(false);			
+		}
+		// catch error
+		worker.onerror = function(event){
+			console.log("DEBUG selectCurrentCustomer: " + event.data);
+		}
+		
+		worker.postMessage([currentId, customers]);
+	}
+}
---
+++ trunk/TrackJavaScript/Start_HTML5/WebContent/start-script/MainController.js
@@ -0,0 +1,47 @@
+/* Resposibilities
+ * - control resource access
+ * - backend services integration
+ * - host global/application state vars (data structures)
+ * 	 - configuration as URLS
+ *   - data model and local persistence 
+ * - control form lifecycle (ex. initialization)
+ */
+
+// CFG: backend services integration configuration
+var urlRestBase = "http://localhost:8080/ProRest_WebBackend/services/customer/json/";
+
+// CFG: data model definition
+var customerService = new CustomerService(urlRestBase);
+var customer;
+var customers;
+
+
+// Form life-cycle: init
+window.onload = function(){
+	console.log("Loading StartForm. " + Date() + " WebWorkers support: " + typeof(Worker));
+	initDataModel();
+	
+};
+// Form life-cycle: ending
+window.onunload = function(){
+	console.log("UnLoading StartForm ... " + Date());
+	// save to localStorage the application state
+	// cleanup resource usage
+}
+
+// Form life-cycle callbacks: data model 
+function initDataModel(){
+	console.log(" - initDataModel ... " + Date());
+	// asynchronous binding form 
+	customerService.getCustomers(bindingForm);
+	// Disable form
+	disableFormContent(true);	
+}
+
+function disableFormContent(enable){
+	document.getElementById("fldsNav").disabled= enable;
+	document.getElementById("fldsId").disabled= enable;
+	document.getElementById("fldsDetails").disabled= enable;
+	document.getElementById("fldsAdress").disabled= enable;
+	document.getElementById("fldsActions").disabled= enable;
+}
