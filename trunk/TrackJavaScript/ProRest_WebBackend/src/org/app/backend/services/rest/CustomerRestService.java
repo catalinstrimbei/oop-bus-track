81a8c73c035ecea93db60bdb6fda7d1ec785c2bc
catalin.strimbei@gmail.com <catalin.strimbei@gmail.com@f7f365bf-cd1a-e18e-2544-695ceb87d913>
2013-04-25 09:17:31 +0000

    Test: XML for SDO Rest Endpoint

--- trunk/TrackJavaScript/ProRest_WebBackend/src/org/app/backend/services/rest/CustomerRestService.java
+++ trunk/TrackJavaScript/ProRest_WebBackend/src/org/app/backend/services/rest/CustomerRestService.java
@@ -1,14 +1,22 @@
 package org.app.backend.services.rest;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 // Plain old Java Object it does not extend as class or implements 
@@ -34,6 +42,7 @@ public class CustomerRestService {
   // This method is called if XML is request
   @GET
   @Produces(MediaType.TEXT_XML)
+  @Path("/getxml")
   public String sayXMLHello() {
     return "<?xml version=\"1.0\"?>" + "<hello> Hello CUSTOMER REST!" + "</hello>";
   }
@@ -78,12 +87,73 @@ public class CustomerRestService {
 	  jsonCustomer.setName(jsonCustomer.getName() + "_");
 	  return jsonCustomer;
   }    
+  
+  
+  @GET
+  @Produces(MediaType.TEXT_XML)
+  @Path("/getxmlcustomers")
+  public String getXMLCustomers() throws IOException {
+	  InputStream fis = Thread.currentThread().getContextClassLoader().getResourceAsStream("sdo/data/customerSDO.xml");
+	  
+	  BufferedReader br = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
+	  StringBuilder sb = new StringBuilder();
+	    try {
+	        String line = br.readLine();
+
+	        while (line != null) {
+	            sb.append(line);
+	            sb.append("\n");
+	            line = br.readLine();
+	        }
+	        String everything = sb.toString();
+	    } finally {
+	        br.close();
+	    }	
+
+	    return sb.toString() ;
+    
+    /*
+     ------------------------
+     //String getXMLCustomers(@Context HttpServletResponse  serv) throws IOException
+	    ServletOutputStream o =serv.getOutputStream();
+	    StringBuffer sb = new StringBuffer();
+	    try {
+	        FileInputStream fis = new FileInputStream(new File("src/sdo/data/customerSDO.xml"));
+	        int c;
+
+	        while ((c = fis.read()) != -1) {
+	           o.write(c);
+	           sb.append(b)
+	        }
+
+	        fis.close();
+	        o.close();
+	    } catch (Exception e) {
+	        System.err.println("e");
+	    } 
+
+	    return "OK" ;
+	    -------------------------     
+      @GET
+      @Produces("multipart/mixed")
+      public MultipartOutput get()
+      {
+         MultipartOutput output = new MultipartOutput();
+         output.addPart(new Customer("bill"), MediaType.APPLICATION_XML_TYPE);
+         output.addPart(new Customer("monica"), MediaType.APPLICATION_XML_TYPE);
+         return output;
+      }    
+    */
+  }  
 } 
 
 
 /*
 	http://localhost:8080/ProRest_WebBackend/services/customer
-	
+	http://localhost:8080/ProRest_WebBackend/services/customer/json/getcustomers
+	http://localhost:8080/ProRest_WebBackend/services/customer/sayXMLHello
+	http://localhost:8080/ProRest_WebBackend/services/customer/getxml
+	http://localhost:8080/ProRest_WebBackend/services/customer/getxmlcustomers
 	
 	http://theopentutorials.com/examples/java-ee/jax-rs/create-a-simple-restful-web-service-using-jersey-jax-rs/
 	http://www.vogella.com/articles/REST/article.html	
