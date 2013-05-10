7f1215b8ce927fd50d2aa9057a578a2384933eef
catalin.strimbei@gmail.com <catalin.strimbei@gmail.com@f7f365bf-cd1a-e18e-2544-695ceb87d913>
2013-05-10 09:11:45 +0000

    Changed MVC version ...

---
+++ trunk/TrackJavaScript/Start_OOJS/WebContent/js-MVC2/Event.js
@@ -0,0 +1,23 @@
+/**
+ * http://www.alexatnet.com/articles/model-view-controller-mvc-javascript
+ * 
+ * JavaScript Event-based Observer Implementation
+ * 
+ */
+function Event(sender) {
+    this._sender = sender;
+    this._listeners = [];
+}
+
+Event.prototype = {
+    attach : function (listener) {
+        this._listeners.push(listener);
+    },
+    notify : function (args) {
+        var index;
+
+        for (index = 0; index < this._listeners.length; index += 1) {
+            this._listeners[index](this._sender, args);
+        }
+    }
+};
