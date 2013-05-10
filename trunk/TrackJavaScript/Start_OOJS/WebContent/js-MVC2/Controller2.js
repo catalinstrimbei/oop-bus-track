74d7b661083d02d113c40f79de8ca0c16ac5ba4c
catalin.strimbei@gmail.com <catalin.strimbei@gmail.com@f7f365bf-cd1a-e18e-2544-695ceb87d913>
2013-05-10 09:18:33 +0000

    Start MVC2 Test

--- trunk/TrackJavaScript/Start_OOJS/WebContent/js-MVC2/Controller2.js
+++ trunk/TrackJavaScript/Start_OOJS/WebContent/js-MVC2/Controller2.js
@@ -4,9 +4,8 @@
  * The Controller. Controller responds to user actions and
  * invokes changes on the model.
  */
-function ListController(model, view) {
+function ListController(model) {
     this._model = model;
-    this._view = view;
 
     var _this = this;
 
--- trunk/TrackJavaScript/Start_OOJS/WebContent/js-MVC2/View2.js
+++ trunk/TrackJavaScript/Start_OOJS/WebContent/js-MVC2/View2.js
@@ -5,8 +5,7 @@
  * the UI events. The controller is attached to these
  * events to handle the user interraction.
  */
-function ListView(model, elements) {
-    this._model = model;
+function ListView(elements) {
     this._elements = elements;
 
     this.listModified = new Event(this);
@@ -15,26 +14,13 @@ function ListView(model, elements) {
 
     var _this = this;
 
-    // attach model listeners
-    this._model.itemAdded.attach(function () {
-        _this.rebuildList();
-    });
-    this._model.itemRemoved.attach(function () {
-        _this.rebuildList();
-    });
-
     // attach listeners to HTML controls
+    console.log('set list.onchange');    
     this._elements.list.onchange = function (e) {
+    	console.log('list.onchange');
         _this.listModified.notify({ index : e.target.selectedIndex });
     };
-    /*
-    this._elements.addButton.click(function () {
-        _this.addButtonClicked.notify();
-    });
-    this._elements.delButton.click(function () {
-        _this.delButtonClicked.notify();
-    });
-    */
+
     console.log('set addButton.onClick');
     this._elements.addButton.onclick = function () {
     	console.log('addButton.onClick');
@@ -52,22 +38,18 @@ ListView.prototype = {
         this.rebuildList();
     },
 
-    rebuildList : function () {
-        var list, items, key;
+    rebuildList : function (items) {
+        var list, key;
 
         list = this._elements.list;
-        //list.html('');
         list.length = 0;
 
-        items = this._model.getItems();
         for (key in items) {
             if (items.hasOwnProperty(key)) {
-                //list.append($('<option>' + items[key] + '</option>'));
             	var opt = document.createElement('option');
             	opt.text = items[key];
             	list.add(opt);
             }
         }
-        this._model.setSelectedIndex(-1);
     }
 };
