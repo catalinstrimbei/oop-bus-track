53e27cd4031dadb772f355a0e00fccc3a88f2d5c
catalin.strimbei <catalin.strimbei@gmail.com>
2013-05-13 08:31:55 +0000

    MVC2 Test Fix

--- TrackJavaScript/Start_OOJS/WebContent/js-MVC2/View2.js
+++ TrackJavaScript/Start_OOJS/WebContent/js-MVC2/View2.js
@@ -5,40 +5,38 @@
  * the UI events. The controller is attached to these
  * events to handle the user interaction.
  */
-function EventListModified(){};
-function EventAddButtonClicked(){};
-function EventDelButtonClicked(){};
+
+function EventListModified(){this.setClass();};	
+function EventAddButtonClicked(){this.setClass();};
+function EventDelButtonClicked(){this.setClass();};
 
 function ListView(elements) {
+
+	
+	
     this._elements = elements;
     var _this = this;
     
-    
+    console.log('start listview events');
     EventListModified.prototype = new Event(_this);
     this.listModified = new EventListModified();
-//	console.log("event type: " + (this.listModified instanceof EventListModified)); 
-    
     
     EventAddButtonClicked.prototype = new Event(_this);
     this.addButtonClicked = new EventAddButtonClicked();
     
-    
     EventDelButtonClicked.prototype = new Event(_this);
     this.delButtonClicked = new EventDelButtonClicked();
-
+    console.log('end listview events');
 
     // attach listeners to HTML controls
-    console.log('set list.onchange');    
     this._elements.list.onchange = function (e) {
     	console.log('list.onchange');
         _this.listModified.notify({ index : e.target.selectedIndex });
     };
-    console.log('set addButton.onClick');
     this._elements.addButton.onclick = function () {
     	console.log('addButton.onClick');
         _this.addButtonClicked.notify();
     };
-    console.log('set delButton.onClick');
     this._elements.delButton.onclick = function () {
     	console.log('delButton.onClick');
         _this.delButtonClicked.notify();
