704ac520d31f18d45a29572099f3e61294dd7291
catalin.strimbei@gmail.com <catalin.strimbei@gmail.com@f7f365bf-cd1a-e18e-2544-695ceb87d913>
2013-11-04 12:10:36 +0000

    Exemple  skeleton

---
+++ trunk/ExempleCursJava/ScrumPOJO/src/org/app/scrum/Test2_IncapsulareAtribute_Proprietati.java
@@ -0,0 +1,12 @@
+package org.app.scrum;
+
+public class Test2_IncapsulareAtribute_Proprietati {
+
+	public static void main(String[] args) {
+		// Instantiere clasa Task, initializare dataStart, timpRamas
+		
+		// access proprietate durataEstimare
+
+	}
+
+}
---
+++ trunk/ExempleCursJava/ScrumPOJO/src/org/app/scrum/Test4_ImportSprint.java
@@ -0,0 +1,16 @@
+package org.app.scrum;
+
+public class Test4_ImportSprint {
+
+	public static void main(String[] args) {
+		// Instantiere proiect
+		// Adaugare Sprint
+		// Adaugare Task
+		
+		// Impachetare sprint si task
+		
+		// Import clase impachetate
+
+	}
+
+}
