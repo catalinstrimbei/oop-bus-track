8b1289f03aa13e65598e66fd9f24430a036ce281
catalin.strimbei <catalin.strimbei@gmail.com>
2014-10-21 09:57:42 +0000

    Scrum Java Courses on SVN

---
+++ ExempleCursJava/ScrumJPA/src/org/course/poo/cap1/Cerinta.java
@@ -0,0 +1,8 @@
+package org.course.poo.cap1;
+
+public class Cerinta{
+	Integer idCerinta;
+	String denumire;
+	String descriere;
+	
+}
---
+++ ExempleCursJava/ScrumJPA/src/org/course/poo/cap1/Sprint.java
@@ -0,0 +1,14 @@
+package org.course.poo.cap1;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Sprint {
+	Integer idSprint;
+	String obiectiv;
+	Date dataStart;
+	String review;
+	
+	List<Cerinta> cerinte = new ArrayList<>();
+}
