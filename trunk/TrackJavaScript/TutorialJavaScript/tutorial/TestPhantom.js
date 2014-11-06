3718eac17e71711fb696cd1c4c1635c706c7406f
catalin.strimbei@gmail.com <catalin.strimbei@gmail.com@f7f365bf-cd1a-e18e-2544-695ceb87d913>
2014-11-06 10:17:43 +0000

    Config PhatomJS runtime mode.

---
+++ trunk/TrackJavaScript/TutorialJavaScript/tutorial/TestPhantom.js
@@ -0,0 +1,7 @@
+/**
+ * 1. Download PhantomJS runtime from http://phantomjs.org/download.html
+ * 2. Set PhantomJS runtime in [Window\Preferences]-Nodeclipse-PhantomJS-[PhantomJS path:]
+ */
+console.log("Hello PhantomJS!");
+
+phantom.exit();
